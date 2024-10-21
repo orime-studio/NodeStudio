6b733166316a2f40e2b2273138237abef24f861f
TamamTamar <tamar.tamam770@gmail.com>
2024-10-21 10:52:05 +0300

    gg

--- src/routes/message-router.ts
+++ src/routes/message-router.ts
@@ -5,16 +5,10 @@ import router from "./users";
 //create new message
 router.post('/send-message', async (req, res, next) => {
     try {
-        const messageData: IMessage = req.body;
-        const result = await messageService.createMessage(messageData);
-        
-        // שליחת מייל לאחר יצירת ההודעה
-        await messageService.sendEmail(messageData);
-        
+        const result = await messageService.createMessage(req.body as IMessage);
         res.status(201).json(result);
     } catch (e) {
-        console.error('Error creating message:', e); // הדפסת שגיאה לקונסול
-        res.status(500).json({ message: 'Error creating message', error: e.message });
+        next(e.message);
     }
 });
 
--- src/services/message-service.ts
+++ src/services/message-service.ts
@@ -1,15 +1,6 @@
 import { IMessage } from '../@types/@types';
 import Message from '../db/models/message-model';
-import nodemailer from 'nodemailer';
 
-// הגדרת הטרנספולר
-const transporter = nodemailer.createTransport({
-    service: 'gmail', // או ספק דוא"ל אחר
-    auth: {
-        user: process.env.EMAIL_USER, // כתובת המייל שלך
-        pass: process.env.EMAIL_PASS, // הסיסמה של המייל שלך
-    },
-});
 
 export const messageService = {
     // יצירת הודעה חדשה
@@ -17,12 +8,7 @@ export const messageService = {
         console.log('Creating a new message with data:', data);
         const message = new Message(data);
         console.log('Message created:', message);
-
-        // שמירת ההודעה במסד הנתונים
-        await message.save();
-
-        // שליחת המייל
-        await messageService.sendEmail(data); // קריאה לשליחת המייל
+        return message.save();
     },
 
     // קבלת כל ההודעות
@@ -31,32 +17,5 @@ export const messageService = {
         const messages = await Message.find({}).sort({ createdAt: -1 });
         console.log('Messages fetched:', messages);
         return messages;
-    },
-
-    // שליחת מייל
-    sendEmail: async (messageData: IMessage) => {
-        console.log('Preparing to send email...');
-        const mailOptions = {
-            from: process.env.EMAIL_USER,
-            to: 'orime.studio.wd@gmail.com',
-            subject: 'New Message from Contact Form',
-            text: `
-            You have received a new message:
-    
-            Name: ${messageData.fullName}
-            Email: ${messageData.email}
-            Phone: ${messageData.phone}
-            Message: ${messageData.message}
-            `,
-        };
-
-        try {
-            console.log('Sending email with options:', mailOptions);
-            await transporter.sendMail(mailOptions);
-            console.log('Email sent successfully');
-        } catch (error) {
-            console.error('Error sending email:', error);
-            throw new Error('Could not send email');
-        }
     },
 };
