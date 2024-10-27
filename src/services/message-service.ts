014ad502daf0249727b4d4fe157d473220480d2a
TamamTamar <tamar.tamam770@gmail.com>
2024-10-27 17:16:59 +0200

    mail

--- src/services/message-service.ts
+++ src/services/message-service.ts
@@ -1,6 +1,26 @@
+// services/message-service.ts
+import nodemailer from 'nodemailer';
 import { IMessage } from '../@types/@types';
 import Message from '../db/models/message-model';
 
+const sendEmail = (to: string, subject: string, text: string) => {
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'orime.studio.wd@gmail.com', // כתובת הדוא"ל שלך
+            pass: 'rgbphd45BN', // סיסמת האפליקציה שלך
+        },
+    });
+
+    const mailOptions = {
+        from: 'orime.studio.wd@gmail.com',
+        to,
+        subject,
+        text,
+    };
+
+    return transporter.sendMail(mailOptions);
+};
 
 export const messageService = {
     // יצירת הודעה חדשה
@@ -8,7 +28,14 @@ export const messageService = {
         console.log('Creating a new message with data:', data);
         const message = new Message(data);
         console.log('Message created:', message);
-        return message.save();
+
+        // שמירת ההודעה במסד הנתונים
+        const savedMessage = await message.save();
+
+        // שליחת המייל
+        await sendEmail(data.email, 'New Message', data.message); // שלחי מייל עם הפרטים
+
+        return savedMessage; // החזרת ההודעה שנשמרה
     },
 
     // קבלת כל ההודעות
