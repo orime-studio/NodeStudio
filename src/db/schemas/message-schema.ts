3d0bf5c709c38b8c1fbf480eaf2fd97f2d39e99c
TamamTamar <tamar.tamam770@gmail.com>
2024-11-12 14:01:27 +0200

    h

--- src/db/schemas/message-schema.ts
+++ src/db/schemas/message-schema.ts
@@ -5,7 +5,7 @@ const MessageSchema = new Schema<IMessage>({
     fullName: { type: String, required: true, minlength: 2, maxlength: 256 },
     email: { type: String, required: true, minlength: 2, maxlength: 256 },
     phone: { type: String, required: true, minlength: 9, maxlength: 15 },
-    message: { type: String, required: false, minlength: 2, maxlength: 256 },
+    message: { type: String, required: true, minlength: 2, maxlength: 256 },
     createdAt: { type: Date, default: Date.now },
 
 });
